39ebb665e8530e3de8e821e14e1d0cf10257ebce
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-11-27 09:45:57 +0100

    dataclass for what I believe will be the result of the Spa-treatment.

---
+++ src/main/kotlin/no/nav/helse/Vedtak.kt
@@ -0,0 +1,24 @@
+package no.nav.helse
+
+import java.math.BigDecimal
+import java.time.LocalDate
+
+/**
+ * Forventet resultat av en søknad.
+ *
+ * Mulig at begrepet "vedtak" er belastet og burde erstattes av noe annet.
+ */
+data class Vedtak(
+        /**utledes av søknad*/
+        val søknadsnr: String,
+        /**prosent-tall*/
+        val gradering: Float,
+        /**inklusiv start-dato for utbetaling. Dette vil typisk være første sykedag etter arbeidsgiverperioden, men det finnes unntak*/
+        val fom: LocalDate,
+        /**inklusiv siste dag for utbetaling. Dette vil være siste dag i sykemelding, siste dag i søknak, eller siste tilgjengelige antall utbetalingsdager overstiger brukerens tilgjengelige antall dager (som vanligvis er 248 dager etter arbeidsgiverperioden, ekslusive helger men inklusive helligdager)*/
+        val tom: LocalDate,
+        /**uavklart om dette er dagsats eller beløp for hele perioden*/
+        val beløp: BigDecimal,
+        /**typisk skal enten søker eller arbeidsgiver få utbetalingen*/
+        val mottaker: String
+)
