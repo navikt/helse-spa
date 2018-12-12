66c1873204590e08502a525379e2bf22088db7a8
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-12-12 10:42:23 +0100

    added startup/shutdown logging

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,11 +1,16 @@
 package no.nav.helse
 
+import org.slf4j.LoggerFactory
 import kotlin.concurrent.thread
+
+private val log = LoggerFactory.getLogger("Spa")
 
 fun main() {
     val spa = SaksbehandlingStream(Environment())
+    log.info("Opening up the Spa")
     spa.start()
     Runtime.getRuntime().addShutdownHook(thread {
+        log.info("Closing the Spa")
         spa.stop()
     })
 }
