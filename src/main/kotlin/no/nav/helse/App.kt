10c4cca12f7a89522b3a164091932e4125e39704
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-12-12 10:49:34 +0100

    removed shutdown-hook, as it _may_ be conflicting with something in the streams-library

--- src/main/kotlin/no/nav/helse/App.kt
+++ src/main/kotlin/no/nav/helse/App.kt
@@ -1,7 +1,6 @@
 package no.nav.helse
 
 import org.slf4j.LoggerFactory
-import kotlin.concurrent.thread
 
 private val log = LoggerFactory.getLogger("Spa")
 
@@ -9,9 +8,5 @@ fun main() {
     val spa = SaksbehandlingStream(Environment())
     log.info("Opening up the Spa")
     spa.start()
-    Runtime.getRuntime().addShutdownHook(thread {
-        log.info("Closing the Spa")
-        spa.stop()
-    })
 }
 
