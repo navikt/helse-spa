bc20766b6f14a59e3c7431045a9e36d36aa31a61
Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
2018-12-12 10:28:55 +0100

    we're not using Avro, so away with the registry-url

--- src/main/kotlin/no/nav/helse/Environment.kt
+++ src/main/kotlin/no/nav/helse/Environment.kt
@@ -4,7 +4,6 @@ data class Environment(
         val username: String? = getEnvVar("SERVICEUSER_USERNAME"),
         val password: String? = getEnvVar("SERVICEUSER_PASSWORD"),
         val bootstrapServersUrl: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
-        val schemaRegistryUrl: String = getEnvVar("KAFKA_SCHEMA_REGISTRY_URL"),
         val httpPort: Int? = null,
         val navTruststorePath: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
         val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD")
