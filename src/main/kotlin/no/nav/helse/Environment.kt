9698d889d31bdf26875dec7a40401849ae32c80d
royag-nav <roy.andre.gundersen@nav.no>
2019-02-18 14:11:31 +0100

    men noen env-variabler trenger jo faktisk ikke å være satt da

--- src/main/kotlin/no/nav/helse/Environment.kt
+++ src/main/kotlin/no/nav/helse/Environment.kt
@@ -5,12 +5,16 @@ data class Environment(
         val password: String = getEnvVar("SERVICEUSER_PASSWORD"),
         val bootstrapServersUrl: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS"),
         val httpPort: Int? = null,
-        val navTruststorePath: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-        val navTruststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
+        val navTruststorePath: String? = getEnvVarOptional("NAV_TRUSTSTORE_PATH"),
+        val navTruststorePassword: String? = getEnvVarOptional("NAV_TRUSTSTORE_PASSWORD"),
         val sparkelBaseUrl: String = getEnvVar("SPARKEL_BASE_URL", "http://localhost:8099"),
         val stsRestUrl: String = getEnvVar("SECURITY_TOKEN_SERVICE_REST_URL")
 )
 
 private fun getEnvVar(varName: String, defaultValue: String? = null) =
-        System.getenv(varName) ?: defaultValue ?: throw Exception("mangler verdi for $varName")
+        getEnvVarOptional(varName, defaultValue) ?: throw Exception("mangler verdi for $varName")
+
+private fun getEnvVarOptional(varName: String, defaultValue: String? = null) =
+        System.getenv(varName) ?: defaultValue
+
 
