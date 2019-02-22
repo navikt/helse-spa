33a4a636075cf97b51d2bda985a6e830c8633915
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2019-02-22 13:05:56 +0100

    mer rydding i medlemsskap

--- src/main/kotlin/no/nav/helse/Medlemsskap.kt
+++ src/main/kotlin/no/nav/helse/Medlemsskap.kt
@@ -4,29 +4,29 @@ import no.nav.nare.core.evaluations.Evaluering
 import no.nav.nare.core.evaluations.Resultat
 import no.nav.nare.core.specifications.Spesifikasjon
 
-val erMedlem = Spesifikasjon<Tpsfakta>(
+val erMedlem = Spesifikasjon<Medlemsskapgrunnlag>(
         beskrivelse = "Formålet med sykepenger er å gi kompensasjon for bortfall av arbeidsinntekt for yrkesaktive medlemmer som er arbeidsuføre på grunn av sykdom eller skade.",
         identitet = "§ 8-1"
-) { tps -> sokerBorI("NOR", tps) }
+) { grunnlag-> sokerBorI("NOR", grunnlag) }
 
-fun sokerBorI(land: String, tps: Tpsfakta): Evaluering =
-        if (tps.bostedland == land)
+fun sokerBorI(land: String, grunnlag: Medlemsskapgrunnlag): Evaluering =
+        if (grunnlag.bostedsland == land)
             Evaluering.ja("Søker er bosatt i $land")
         else
-            Evaluering.nei("Søker er bosatt i ${tps.bostedland} som ikke er $land")
+            Evaluering.nei("Søker er bosatt i ${grunnlag.bostedsland} som ikke er $land")
 
 data class Medlemsskapgrunnlag(val bostedsland: String)
 
 fun vurderMedlemskap(soknad: BeriketSykepengesøknad): Vurdering<Boolean, Medlemsskapgrunnlag> {
+    val grunnlag = Medlemsskapgrunnlag(soknad.faktagrunnlag.tps.bostedland)
     val evaluering: Evaluering = with(erMedlem) {
-        evaluer(soknad.faktagrunnlag.tps).also {
+        evaluer(grunnlag).also {
             narePrometheus.tellEvaluering { it }
         }
     }
 
     return when (evaluering.resultat) {
-        Resultat.KANSKJE -> Vurdering.Uavklart(Vurdering.Uavklart.Arsak.SKJONN, evaluering.begrunnelse, Medlemsskapgrunnlag(soknad.faktagrunnlag.tps.bostedland))
-        else -> Vurdering.Avklart(evaluering.resultat == Resultat.JA, evaluering.begrunnelse, Medlemsskapgrunnlag(soknad.faktagrunnlag.tps.bostedland)
-                , "SPA")
+        Resultat.KANSKJE -> Vurdering.Uavklart(Vurdering.Uavklart.Arsak.SKJONN, evaluering.begrunnelse, grunnlag)
+        else -> Vurdering.Avklart(evaluering.resultat == Resultat.JA, evaluering.begrunnelse, grunnlag, "SPA")
     }
 }
