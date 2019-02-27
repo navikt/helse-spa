e390fde568934492655b22e6a696acf7b3554bfa
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-02-27 14:31:16 +0100

    Update inngangsvilkår version

--- src/main/kotlin/no/nav/helse/Vilkårsvurdering.kt
+++ src/main/kotlin/no/nav/helse/Vilkårsvurdering.kt
@@ -7,18 +7,14 @@ import no.nav.nare.core.evaluations.Evaluering
 
 fun gjennomførVilkårsvurdering(søknad: AvklartSykepengesoknad): Evaluering {
     val grunnlag = Vilkårsgrunnlag(
-            førsteSykdomsdag = søknad.originalSøknad.startSyketilfelle,
-            datoForAnsettelse = søknad.arbeidsforhold.grunnlag.arbeidsgivere.first().startdato, // this is not right
+            opptjeningstid = (søknad.opptjeningstid as Vurdering.Avklart).fastsattVerdi.toInt(), // this is not right
             alder = (søknad.alder as Vurdering.Avklart).fastsattVerdi,
             erMedlem = (søknad.medlemsskap as Vurdering.Avklart).fastsattVerdi,
             ytelser = emptyList(),
             søknadSendt = søknad.originalSøknad.sendtNav!!.toLocalDate(),
             førsteDagSøknadGjelderFor = søknad.originalSøknad.fom,
-            aktuellMånedsinntekt = 10000L,
-            rapportertMånedsinntekt = 10000L,
             fastsattÅrsinntekt = (søknad.sykepengegrunnlag as Vurdering.Avklart).fastsattVerdi.sykepengegrunnlagNårTrydenYter.fastsattVerdi,
-            grunnbeløp = 96883,
-            harVurdertInntekt = false
+            grunnbeløp = 96883
     )
 
     return narePrometheus.tellEvaluering { sykepengevilkår.evaluer(grunnlag) }
