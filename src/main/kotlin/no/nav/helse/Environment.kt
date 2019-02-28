818c857e3f043b8ebcc3f144a69068fb4e26c8de
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2019-02-28 09:42:14 +0100

    corrected default value

--- src/main/kotlin/no/nav/helse/Environment.kt
+++ src/main/kotlin/no/nav/helse/Environment.kt
@@ -9,7 +9,7 @@ data class Environment(
         val httpPort: Int? = null,
         val navTruststorePath: String? = getEnvVarOptional("NAV_TRUSTSTORE_PATH"),
         val navTruststorePassword: String? = getEnvVarOptional("NAV_TRUSTSTORE_PASSWORD"),
-        val sparkelBaseUrl: String = getEnvVar("SPARKEL_BASE_URL", "http://localhost:8099"),
+        val sparkelBaseUrl: String = getEnvVar("SPARKEL_BASE_URL", "http://sparkel"),
         val stsRestUrl: String = getEnvVar("SECURITY_TOKEN_SERVICE_REST_URL"),
         val plainTextKafka: String? = getEnvVarOptional("PLAIN_TEXT_KAFKA")
 )
