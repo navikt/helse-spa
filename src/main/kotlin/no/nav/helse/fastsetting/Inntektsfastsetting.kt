97e7de84b739c62decab088f15a74cba5ddf0e0b
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-04-11 14:22:37 +0200

    det er SPA som ikke forstår dataene, ikke loven som krever skjønn

--- src/main/kotlin/no/nav/helse/fastsetting/Inntektsfastsetting.kt
+++ src/main/kotlin/no/nav/helse/fastsetting/Inntektsfastsetting.kt
@@ -62,7 +62,7 @@ fun fastsettingAvSykepengegrunnlagetIArbeidsgiverperioden(førsteSykdomsdag: Loc
         return if (beregningsperiode.inntekter.keys.firstOrNull { it == arbeidsgiver.orgnummer } == null) {
             Uavklart(HAR_IKKE_DATA, "Kan ikke avklare sykepengegrunnlaget fordi det finnes ikke inntekter fra aktuell arbeidsgiver", beregningsperiode)
         } else {
-            Uavklart(KREVER_SKJØNNSMESSIG_VURDERING, "Kan ikke avklare sykepengegrunnlaget fordi det andre inntekter i arbeidsgiverperioden i tillegg til aktuell arbeidsgiver", beregningsperiode)
+            Uavklart(FORSTÅR_IKKE_DATA, "Kan ikke avklare sykepengegrunnlaget fordi det andre inntekter i arbeidsgiverperioden i tillegg til aktuell arbeidsgiver", beregningsperiode)
         }
     }
 
