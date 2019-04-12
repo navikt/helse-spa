92643b58b4b087575c871a1fa05adb1c0727ef0a
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-04-12 19:29:15 +0200

    splitt sjekk på arbeidsforhold og sluttdato

--- src/main/kotlin/no/nav/helse/fastsetting/Arbeidsforhold.kt
+++ src/main/kotlin/no/nav/helse/fastsetting/Arbeidsforhold.kt
@@ -2,6 +2,7 @@ package no.nav.helse.fastsetting
 
 import no.nav.helse.behandling.FaktagrunnlagResultat
 import no.nav.helse.domain.Arbeidsforhold
+import no.nav.helse.fastsetting.Vurdering.Uavklart
 
 fun vurderArbeidsforhold(fakta : FaktagrunnlagResultat) : Vurdering<Boolean, List<Arbeidsforhold>> {
     val arbeidsforholdFakta = fakta.faktagrunnlag.arbeidsforhold
@@ -9,11 +10,13 @@ fun vurderArbeidsforhold(fakta : FaktagrunnlagResultat) : Vurdering<Boolean, Lis
     return when {
         arbeidsforholdFakta.any {
             it.type != "Arbeidstaker"
-        } -> Vurdering.Uavklart(Vurdering.Uavklart.Årsak.FALLER_UTENFOR_MVP, "Søker har frilansarbeidsforhold", arbeidsforholdFakta)
-        arbeidsforholdFakta.any {
+        } -> Uavklart(Uavklart.Årsak.FALLER_UTENFOR_MVP, "Søker har frilansarbeidsforhold", arbeidsforholdFakta)
+        arbeidsforholdFakta.none {
             it.arbeidsgiver.identifikator == fakta.originalSøknad.arbeidsgiver.orgnummer
-                    && it.sluttdato == null
-        } -> Vurdering.Avklart(true, "Søker har et aktivt arbeidsforhold hos ${fakta.originalSøknad.arbeidsgiver.navn}", arbeidsforholdFakta, "SPA")
-        else -> Vurdering.Uavklart(Vurdering.Uavklart.Årsak.FORSTÅR_IKKE_DATA, "Søker har ikke aktivt arbeidsforhold hos ${fakta.originalSøknad.arbeidsgiver.navn}", arbeidsforholdFakta)
+        } -> Uavklart(Uavklart.Årsak.HAR_IKKE_DATA, "Søker har ikke arbeidsforhold hos ${fakta.originalSøknad.arbeidsgiver.navn}", arbeidsforholdFakta)
+        arbeidsforholdFakta.first {
+            it.arbeidsgiver.identifikator == fakta.originalSøknad.arbeidsgiver.orgnummer
+        }.sluttdato != null -> Uavklart(Uavklart.Årsak.FALLER_UTENFOR_MVP, "Søker har sluttdato hos ${fakta.originalSøknad.arbeidsgiver.navn}", arbeidsforholdFakta)
+        else -> Vurdering.Avklart(true, "Søker har et aktivt arbeidsforhold hos ${fakta.originalSøknad.arbeidsgiver.navn}", arbeidsforholdFakta, "SPA")
     }
 }
