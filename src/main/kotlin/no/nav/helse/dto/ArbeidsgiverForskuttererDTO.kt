4b3d342c2e1eb502b632dc95f3a6c1f440fbd536
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-04-30 21:30:35 +0200

    Revert "sørg for en standardverdi når ArbeidsgiverForskutterer er null"
    This reverts commit fda7931e9a7e50fb3428967c9825ae1d2d84f88d.

--- src/main/kotlin/no/nav/helse/dto/ArbeidsgiverForskuttererDTO.kt
+++ src/main/kotlin/no/nav/helse/dto/ArbeidsgiverForskuttererDTO.kt
@@ -1,11 +1,7 @@
 package no.nav.helse.dto
 
-import com.fasterxml.jackson.annotation.JsonEnumDefaultValue
-
 enum class ArbeidsgiverForskuttererDTO {
     JA,
     NEI,
-    VET_IKKE,
-    @JsonEnumDefaultValue
-    UKJENT
+    VET_IKKE
 }
