5f60aee9fbf14cecda1d9ca62b09f6ec36db22e1
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-05-07 15:46:40 +0200

    tell de med uten arbeidsforhold

--- src/main/kotlin/no/nav/helse/behandling/mvp/Opptjeningstid.kt
+++ src/main/kotlin/no/nav/helse/behandling/mvp/Opptjeningstid.kt
@@ -5,6 +5,7 @@ import no.nav.helse.oppslag.arbeidinntektytelse.dto.ArbeidsforholdDTO
 
 fun vurderMVPKriterierForOpptjeningstid(arbeidsgiverFraSøknad: ArbeidsgiverFraSøknad, arbeidsforhold: List<ArbeidsforholdDTO>): MVPFeil? {
     return when {
+        arbeidsforhold.isEmpty() -> MVPFeil("Ingen arbeidsforhold", "Søker har ingen arbeidsforhold og vi forventer kun 1")
         arbeidsforhold.size != 1 -> MVPFeil("Mer enn ett arbeidsforhold", "Søker har ${arbeidsforhold.size} arbeidsforhold og vi forventer kun 1")
         arbeidsgiverFraSøknad.orgnummer != arbeidsforhold[0].arbeidsgiver.identifikator -> MVPFeil("Finner ikke arbeidsforhold fra søknad", "Finner ikke arbeidsforholdet søker har opplyst om i søknaden i listen over registrerte arbeidsforhold")
         else -> null
